adc214babd9cda09da6ff92f78a4297dd9d99919
stembrino <stembrino@gmail.com>
2023-08-27 14:10:12 +0100

    fix style outline btn

--- src/app/fixed-income/_components/Stateless/Button/Button.tsx
+++ src/app/fixed-income/_components/Stateless/Button/Button.tsx
@@ -13,12 +13,12 @@ type ButtonProps = {
 }
 
 const variants = tv({
-  base: "disabled:bg-gray-300 rounded-full",
+  base: "rounded-full disabled:bg-gray-100 disabled:cursor-not-allowed",
   variants: {
     variant: {
       primary: "bg-zinc-700 hover:bg-zinc-600 active:bg-zinc-900 text-white",
       secondary: "bg-sky-700 hover:bg-sky-600 active:bg-sky-800 text-white",
-      outlined: "bg-transparent border border-zinc-700 hover:bg-zinc-700 hover:text-white active:bg-zinc-900"
+      outlined: "bg-transparent border border-zinc-700 text-black hover:outline-1 hover:outline active:outline-2"
     },
     size: {
       small: "text-xs py-1 px-2 sm:text-sm sm:px-3 sm:py-1",
